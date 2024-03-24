76e8af25a16c7189f8280deab8efe54ade950704
Yassine <153839691+Mastersonic544@users.noreply.github.com>
2024-03-24 06:59:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let allCards = ['card1.png', 'card2.png', 'card3.png', 'card4.png', 'card5.png', 'card6.png', 'card7.png', 'card8.png', 'card9.png', 'card10.png', 'card11.png', 'card12.png', 'card13.png', 'card14.png', 'card15.png', 'card16.png', 'card17.png', 'card18.png', 'card19.png', 'card20.png', 'card21.png', 'card22.png', 'card23.png', 'card24.png', 'card25.png', 'card26.png', 'card27.png', 'card28.png', 'card29.png', 'card30.png', 'card31.png', 'card32.png', 'card33.png', 'card34.png', 'card35.png', 'card36.png', 'card37.png', 'card38.png', 'card39.png', 'card40.png', 'card41.png', 'card42.png', 'card43.png', 'card44.png', 'card45.png', 'card46.png', 'card47.png', 'card48.png', 'card49.png', 'card50.png', 'card51.png', 'card52.png'];
+let currentCards = [];
+
+function click(bt) { 
+    var temp = document.getElementById(bt);
+    var x = window.getComputedStyle(temp).getPropertyValue("opacity");
+   if (x == 1) {
+        document.getElementById(bt).style.opacity = 0.5;
+    }else{
+        document.getElementById(bt).style.opacity = 1;
+    }
+};
+
+function check(element) {
+    return currentCards.includes(element);
+}
+
+function randomimg() {
+    let selected;
+    do {
+        selected = Math.floor(Math.random() * allCards.length);
+    } while (check(allCards[selected]));
+
+    currentCards.push(allCards[selected]);
+    return allCards[selected];
+}
+function beginning(){
+    for (let i = 1; i <= 25; i++) {
+        document.getElementById('bt' + i).style.backgroundImage = "url(assets/" + randomimg() + ")";
+    }
+}
